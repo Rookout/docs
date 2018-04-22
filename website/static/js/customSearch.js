32e7073e08fbc00bee251473911d5cc30d4f29c9
Mickael Alliel <mickael.alliel@gmail.com>
2018-04-22 18:00:54 +0300

    Updated design of result searches (#20)

--- website/static/js/customSearch.js
+++ website/static/js/customSearch.js
@@ -28,6 +28,7 @@ window.addEventListener('load', function () {
   docsearch({
     apiKey: 'c4a6a6e1d94fba2757ec2969d13ac547',
     indexName: 'rookout',
-    inputSelector: '#rookout-search'
+    inputSelector: '#rookout-search',
+    debug: true
   });
 });
