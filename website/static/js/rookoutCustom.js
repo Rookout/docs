a5fa58d4a0ec50f99457786e93b813bbe168e34f
Mickael Alliel <mickael@rookout.com>
2018-10-31 17:06:58 +0200

    Updated styling of org info

--- website/static/js/rookoutCustom.js
+++ website/static/js/rookoutCustom.js
@@ -76,7 +76,7 @@ function setRookoutTokenInPage(data) {
 
     if (token) {
       body.html(body.html().replace(/\[Your Rookout Token\]/g, token));
-      $('.rookout-org-info').html(`You are currently logged in to <b>${org_name}</b> organization. Do NOT share your private token.`);
+      $('.rookout-org-info').html(`Showing configuration for <b>${org_name}</b>. Do NOT share your private token.`);
     } else {
       error = true;
     }
