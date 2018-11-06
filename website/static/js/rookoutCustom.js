b0d60c960c2eece3c4dfe8c2a641dd89610fad7d
Mickael Alliel <mickael@rookout.com>
2018-11-06 12:49:07 +0200

    Updated search input modifications to jquery

--- website/static/js/rookoutCustom.js
+++ website/static/js/rookoutCustom.js
@@ -10,42 +10,52 @@ gtag('config', 'UA-104510371-3');
 
 
 $(function () {
-  const originalSearchContainer = document.querySelector("ul.nav-site > .navSearchWrapper.reactNavSearchWrapper");
-
-  if (originalSearchContainer !== null) {
-    originalSearchContainer.parentElement.removeChild(originalSearchContainer);
-  }
-
-  const docNavbarElem = document.querySelector("section.navWrapper.wrapper");
-
-  if (docNavbarElem !== null) {
-    const searchContainer = document.createElement('div');
-    searchContainer.classList.add('rook-searchBar', 'rook-docNav');
-
-    const searchBarInput = document.createElement('input');
-    searchBarInput.setAttribute('type', 'text');
-    searchBarInput.setAttribute('placeholder', 'Search...');
-    searchBarInput.setAttribute('id', 'rookout-search');
-
-    const searchBarIcon = document.createElement('img');
-    searchBarIcon.setAttribute('src', '/img/icons/search.svg');
-
-    searchContainer.appendChild(searchBarInput);
-    searchContainer.appendChild(searchBarIcon);
-
-    docNavbarElem.insertBefore(searchContainer, docNavbarElem.children.item(0));
-  }
-
-  docsearch({
-    apiKey: 'c4a6a6e1d94fba2757ec2969d13ac547',
-    indexName: 'rookout',
-    inputSelector: '#rookout-search'
-  });
+  customizeSearchInput();
 
   loadRookoutToken();
 
   setTimeout(loadTabsForOS, 1000);
 });
+
+
+function customizeSearchInput() {
+  const originalSearchContainer = $("ul.nav-site > .navSearchWrapper.reactNavSearchWrapper");
+
+  if (originalSearchContainer.length > 0) {
+    originalSearchContainer.remove();
+  }
+
+  const docNavbarElem = $("section.navWrapper.wrapper");
+
+  if (docNavbarElem.length > 0) {
+    const searchContainer = $(document.createElement('div'));
+    searchContainer.addClass('rook-searchBar').addClass('rook-docNav');
+
+    const searchBarInput = $(document.createElement('input'));
+    searchBarInput.attr('type', 'text');
+    searchBarInput.attr('placeholder', 'Search...');
+    searchBarInput.attr('id', 'rookout-search');
+
+    const searchBarIcon = $(document.createElement('img'));
+    searchBarIcon.attr('src', '/img/icons/search.svg');
+
+    searchContainer.append(searchBarInput);
+    searchContainer.append(searchBarIcon);
+
+    docNavbarElem.prepend(searchContainer);
+  }
+
+  activateAlgoliaDocSearch();
+}
+
+
+function activateAlgoliaDocSearch() {
+  docsearch({
+    apiKey: 'c4a6a6e1d94fba2757ec2969d13ac547',
+    indexName: 'rookout',
+    inputSelector: '#rookout-search'
+  });
+}
 
 
 function loadRookoutToken() {
