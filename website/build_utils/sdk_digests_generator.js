b85fb2284ccb32d17f106707a24fe3a3757e0dcb
Mickael Alliel <mickael@rookout.com>
2019-03-14 12:53:33 +0200

    Added existing digests Added script to write to MD

---
+++ website/build_utils/sdk_digests_generator.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+
+
+function loadSdkDigests() {
+  for (let lang of ["python", "node", "java"]) {
+    setDigestInfoForLang(lang);
+  }
+}
+
+
+function setDigestInfoForLang(lang) {
+  const digestData = JSON.parse(fs.readFileSync(path.join(process.cwd(), "sdk_digests", `digests-${lang}.json`)));
+
+  const divLangStringToReplace = `<div id="${lang}-digests"></div>`;
+
+  const startTable =  '<table>\n' +
+                      '<thead>\n' +
+                      '<tr>\n' +
+                      '<th>Version</th>\n' +
+                      '<th>Digest</th>\n' +
+                      '</tr>\n' +
+                      '</thead>' +
+                      '<tbody>';
+
+  const endTable =    '</tbody></table>';
+
+  let ordered = {};
+  Object.keys(digestData).sort(function(a, b) {
+    // Descending order
+    return semverBigger(b, a);
+  }).forEach(function(key) {
+    ordered[key] = digestData[key];
+  });
+
+  let digestsRows = "";
+  for (let version in ordered) {
+    digestsRows += "<tr>";
+    if (lang === 'python') {
+      digestsRows += `<td>[SHA256] ${digestData[version]['digests'][`rook-${version}.tar.gz`]['sha256']}</td>`;
+    } else if (lang === 'node') {
+      digestsRows += `<td>[SHA512] ${digestData[version]['digests']['integrity']}</td>`;
+    } else if (lang === 'java') {
+      digestsRows += `<td>[SHA1] ${digestData[version]['digests']['sha1']}</td>`;
+    }
+    digestsRows += `</tr>`;
+  }
+
+  const fullTable = startTable + digestsRows + endTable;
+  let baseMd = fs.readFileSync(path.join(process.cwd(), "sdk-digests-base.md"));
+  baseMd.replace(divLangStringToReplace, `<div id="${lang}-digests">${fullTable}</div>`);
+  fs.writeFileSync(path.join(process.cwd(), "sdk-digests-base.md"), baseMd);
+}
+
+
+function semverBigger (a, b) {
+  var pa = a.split('.');
+  var pb = b.split('.');
+  for (var i = 0; i < 3; i++) {
+    var na = Number(pa[i]);
+    var nb = Number(pb[i]);
+    if (na > nb) return 1;
+    if (nb > na) return -1;
+    if (!isNaN(na) && isNaN(nb)) return 1;
+    if (isNaN(na) && !isNaN(nb)) return -1;
+  }
+  return 0;
+}
+
+loadSdkDigests();
