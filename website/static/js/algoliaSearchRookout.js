a1f771fc9b5568f3fe67faf56d33e7d9c1e95f80
Mickael Alliel <mickael@rookout.com>
2020-01-19 15:39:43 +0200

    RK-4713 Set up multiple firebase targets to deploy 2 both rookout and appd website (#288)
    * Set up multiple firebase targets to deploy 2 both rookout and appd website
    
    * Add appd logo asset
    
    * Add another appd logo
    
    * Support multiple algolia indexes depending on deploy target
    Support markdown variable injection for different support email
    Support multiple configuration depending on deploy target by env variable
    Builds and deploy to both targets in circleci
    
    * Fix project name

---
+++ website/static/js/algoliaSearchRookout.js
@@ -0,0 +1,15 @@
+function activateAlgoliaDocSearch() {
+  docsearch({
+    apiKey: 'c4a6a6e1d94fba2757ec2969d13ac547',
+    indexName: 'rookout',
+    inputSelector: '#rookout-search',
+    debug: window.location.hostname === 'localhost'
+  });
+
+  $('#rookout-search-icon').toggle();
+}
+
+
+$(function () {
+  customizeSearchInput(activateAlgoliaDocSearch);
+});
