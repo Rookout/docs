42a4557208b0de9b92f77d60c1773de9d2a77e89
Ryan Hoffman <hopemanryan@gmail.com>
2022-09-05 09:08:56 +0300

    remove docusarus file as its recreated on build time

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/ryan/Desktop/projects/docs/website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/ryan/Desktop/projects/docs/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/ryan/Desktop/projects/docs/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/ryan/Desktop/projects/docs/website/src/css/custom.css'),
-];
