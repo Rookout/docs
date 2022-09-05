d9fedd2b7daecc7e2850535f0568b96895e4fabf
Ryan Hoffman <hopemanryan@gmail.com>
2022-09-05 09:02:28 +0300

    Update fix_swagger_url.js

--- website/build_utils/fix_swagger_url.js
+++ website/build_utils/fix_swagger_url.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 const process = require('process');
-const defaultSwaggerUrl = "https://petstore.swagger.io/v2/swagger.json"
-const rookOutSwaggerUrl = "https://app.rookout.com/api/v1/rookout.yaml"
+const defaultSwaggerUrl = "url: \"https://petstore.swagger.io/v2/swagger.json\","
+
+const RookoutSwaggerDef = `
+      urls: [{
+          url: "https://app.rookout.com/api/v1/rookout.yaml",
+          name: 'RookoutV1'
+        }, {
+          url: "https://app.rookout.com/api/v2/rookout.yaml",
+          name: 'RookoutV2'
+        }],
+          "urls.primaryName": "RookoutV1",
+
+`
 
 const filePath = path.join(process.cwd(), "build","docs", "api", `index.html`)
 let swaggerHtmlFile = fs.readFileSync(filePath, {encoding: 'utf-8'})
-swaggerHtmlFile = swaggerHtmlFile.replace(defaultSwaggerUrl, rookOutSwaggerUrl)
+swaggerHtmlFile = swaggerHtmlFile.replace(defaultSwaggerUrl, RookoutSwaggerDef)
 fs.writeFileSync(filePath, swaggerHtmlFile)
+
 
